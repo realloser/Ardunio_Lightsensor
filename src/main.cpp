3458aa0fb330382716427439692b23341afa2c49
Martyn Messerli <realloser@gmx.ch>
2018-06-25 22:18:23 +0200

    Use mapping for 3.3v support

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
 
 
-#define  Analog0 A0
+#define  ANALOGE_0 A0
+#define  INPUT_VOLTAGE 5
 
-int lightSensorPin = Analog0; // select the input pin for LDR
+int lightSensorPin = ANALOGE_0; // select the input pin for LDR
 
-int sensorValue = 0; // variable to store the value coming from the sensor
+int sensorValue = 0, mappedValue = 0; // variable to store the value coming from the sensor
 
 void setup() {
   Serial.begin(9600); //sets serial port for communication
 }
 void loop() {
   sensorValue = analogRead(lightSensorPin); // read the value from the sensor
-  Serial.println(sensorValue); //prints the values coming from the sensor on the screen
+  mappedValue = map(sensorValue, 0, INPUT_VOLTAGE, 0, 5);
+  Serial.println(mappedValue); //prints the values coming from the sensor on the screen
 
   delay(100);
 
